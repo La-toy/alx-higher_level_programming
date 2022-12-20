c1b9736a723b667321aad9cc5b78d061192affeb
Latoya Witbooi <111246803+La-toy@users.noreply.github.com>
2022-12-20 20:52:33 +0200

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -8,7 +8,6 @@ class Square:
 
     def __init__(self, size):
         """Initialize a new square.
-        
         Args:
             size (int): The size of the new square.
         """
@@ -20,9 +19,9 @@ class Square:
         return (self.__size)
 
     @size.setter
-    def size(size, value):
+    def size(self, value):
         if not isinstance(value, int):
-            raise TypeError("size must be an interger")
+            raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
@@ -32,9 +31,10 @@ class Square:
         return (self.__size * self.__size)
 
     def my_print(self):
-        """Print the square with # character."""
+        """Print the square with the # character."""
         for i in range(0, self.__size):
             [print("#", end="") for j in range(self.__size)]
             print("")
         if self.__size == 0:
             print("")
+      
